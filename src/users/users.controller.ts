a7a399e42b1204f35a90e4d7aaf9fbac6e5c85e3
Najim Uddin <engr.najim3@gmail.com>
2024-09-07 19:48:47 +0600

    # Additional request components

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,16 +3,16 @@ import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 @Controller('users')
 export class UsersController {
   @Get('/:id/:optional?')
-  public getUsers(@Param() params: any, @Query() query: any) {
-    console.log(params);
-    console.log(query);
+  public getUsers(@Param('id') id: any, @Query('limit') limit: any) {
+    console.log(id);
+    console.log(limit);
 
     return 'You sent a get request to users endpoints';
   }
 
   @Post()
-  public createUsers(@Body() request: any) {
-    console.log(request);
+  public createUsers(@Body('email') email: any) {
+    console.log(email);
 
     return 'You sent a post request to users endpoints';
   }
