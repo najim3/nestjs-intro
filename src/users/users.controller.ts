54a883d42e823571036f55e5907443faed06708f
Najim Uddin <engr.najim3@gmail.com>
2024-09-08 15:04:23 +0600

    connecting DTO to route method

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,7 +9,9 @@ import {
   Query,
   ParseIntPipe,
   DefaultValuePipe,
+  ValidationPipe,
 } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -26,14 +28,8 @@ export class UsersController {
   }
 
   @Post()
-  public createUsers(
-    @Body() request: any,
-    @Headers() headers: any,
-    @Ip() ip: any,
-  ) {
-    console.log(request);
-    console.log(headers);
-    console.log(ip);
+  public createUsers(@Body(new ValidationPipe()) createUserDto: CreateUserDto) {
+    console.log(createUserDto);
 
     return 'You sent a post request to users endpoints';
   }
