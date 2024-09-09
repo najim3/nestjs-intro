530aea3414398b1da41b303d914eed702e02e8f4
Najim Uddin <engr.najim3@gmail.com>
2024-09-09 23:11:03 +0600

    Adding configuration methods to swagger

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,14 @@ async function bootstrap() {
   );
 
   // Swagger configuration
-  const config = new DocumentBuilder().setVersion('1.0').build();
+  const config = new DocumentBuilder()
+    .setDescription('Use the base API URL as http://localhost:3000')
+    .setTermsOfService('http://localhost:3000/terms-of-service')
+    .setLicense('MIT Licence', 'https://github.com/najim3/nestjs-intro.git')
+    .addServer('http://localhost:3000')
+    .setTitle('Blog App API')
+    .setVersion('1.0')
+    .build();
   // instantiate document
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
