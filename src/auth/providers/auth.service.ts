2e0cccf02a7679f55a90e7f74ee2441f13127b86
Najim Uddin <engr.najim3@gmail.com>
2024-09-09 19:52:34 +0600

    Circular dependency injection

--- src/auth/providers/auth.service.ts
+++ src/auth/providers/auth.service.ts
@@ -1,4 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject, forwardRef } from '@nestjs/common';
+import { UsersService } from 'src/users/providers/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    // injecting Userservice
+    @Inject(forwardRef(() => UsersService))
+    private readonly userService: UsersService,
+  ) {}
+  public login(email: string, password: string, id: string) {
+    // check user exists database
+    const user = this.userService.findOneById('1234');
+    // login
+    // token
+    return 'SAMPLE_TOKEN';
+  }
+
+  public isAuth() {
+    return true;
+  }
+}
--- src/users/providers/users.service.ts
+++ src/users/providers/users.service.ts
@@ -1,13 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { forwardRef, Inject, Injectable } from '@nestjs/common';
 import { GetUsersParamDto } from '../dtos/get-user-param.dto';
+import { AuthService } from 'src/auth/providers/auth.service';
 
 @Injectable()
 export class UsersService {
+  constructor(
+    // injecting auth service
+    @Inject(forwardRef(() => AuthService))
+    private readonly authService: AuthService,
+  ) {}
   public findAll(
     getUserParamDto: GetUsersParamDto,
     limit: number,
     page: number,
   ) {
+    const isAuth = this.authService.isAuth();
+    console.log(isAuth);
+
     return [
       {
         firstName: 'John',
